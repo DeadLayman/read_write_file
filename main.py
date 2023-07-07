cf3305e7424384f060c3e6e8ffea9576462774f9
Ruslan Vodopyanov <deadlayman@icloud.com>
2023-07-07 19:43:24 +0300

    Task 2: create func "shop_list"

--- main.py
+++ main.py
@@ -4,14 +4,28 @@ with open('recipes.txt', 'r') as recipes:
         if rec == '\n':
             continue
         else:
-            dish_list = []
+            list_dict = []
             name_recipes = rec.strip()
             count_ing = int(recipes.readline())
             for ing in range(count_ing):
                 ingredient = recipes.readline().split('|')
-                dish_dict = {'ingredient_name': ingredient[0],
-                             'quantity': ingredient[1],
-                             'measure': ingredient[2].strip()}
-                dish_list.append(dish_dict)
-                cook_book[name_recipes] = dish_list
+                ing_dict = {'ingredient_name': ingredient[0],
+                            'quantity': ingredient[1],
+                            'measure': ingredient[2].strip()}
+                list_dict.append(ing_dict)
+                cook_book[name_recipes] = list_dict
     print(cook_book)
+
+
+def shop_list(dishes, person_count):
+    amount_ingredients = {}
+    for dish in dishes:
+        if dish in cook_book.keys():
+            for i in cook_book.get(dish):
+                dict_count = {'quantity': int(i.get('quantity')) * person_count, 'measure': i.get('measure')}
+                amount_ingredients[i.get('ingredient_name')] = dict_count
+    print(amount_ingredients)
+
+
+print()
+shop_list(['Запеченный картофель', 'Омлет'], 5)
